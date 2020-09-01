1eb19c64b3e9af1bf47e604cfc982febc2943f57
lukasz <2277lukas@gmail.com>
2020-09-02 00:28:46 +0200

    Poprawiony widok strony logowania i głównej

--- src/main/java/pl/sda/meetup/myownmeetup/controller/HomePageController.java
+++ src/main/java/pl/sda/meetup/myownmeetup/controller/HomePageController.java
@@ -1,5 +1,6 @@
 package pl.sda.meetup.myownmeetup.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,10 @@ import pl.sda.meetup.myownmeetup.dto.EventDto;
 import pl.sda.meetup.myownmeetup.service.EventServiceImpl;
 import pl.sda.meetup.myownmeetup.service.UserServiceImpl;
 
+import java.util.List;
+
 @Controller
+@Slf4j
 public class HomePageController {
 
     private final EventServiceImpl eventService;
@@ -26,6 +30,11 @@ public class HomePageController {
                                 eventService.listOfEventsDatesValidation(
                                         eventService.findAllEvents()))));
         model.addAttribute("userEmail", userService.getLoggedUserName());
+//        List<EventDto> listOfEventsDto = eventService.getListOfEventsDto(
+//                eventService.sortsListOfEventsModels(
+//                        eventService.listOfEventsDatesValidation(
+//                                eventService.findAllEvents())));
+//        log.info("To jest lista wyciągana do indexPage: " + listOfEventsDto.get(0).getTitle() );
         return "homePage";
     }
 
